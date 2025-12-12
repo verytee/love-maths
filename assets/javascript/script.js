4fdc91b21beddca64f4ff33904f382274765f795
Verity Allison <verityalls@gmail.com>
2025-12-12 14:29:42 +0000

    add boilerplate, link script and css and test working

---
+++ assets/javascript/script.js
@@ -0,0 +1,9 @@
+//blue border when about section is clicked
+
+const aboutSection = document.querySelector('section');
+
+aboutSection.addEventListener('click', function() {
+    this.style.border = '3px solid blue';
+});
+
+
