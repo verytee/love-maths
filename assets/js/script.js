ee5df6198cc8b2f42757bd3afa7b56a90369b441
Verity Allison <verityalls@gmail.com>
2025-12-12 15:27:15 +0000

    add fonts and basic structure

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-//blue border when about section is clicked
-
-const aboutSection = document.querySelector('section');
-
-aboutSection.addEventListener('click', function() {
-    this.style.border = '3px solid blue';
-});
-
-
