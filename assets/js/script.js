1778bf1e78165d4af4fcaba70f21b60131e9fc23
Verity Allison <verityalls@gmail.com>
2025-12-15 15:41:33 +0000

    add data-type event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+// event listener for DOM content loaded
+// wait for the DOM to finish loading before running the game
+// get the buttons and add event listeners to them
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+if (this.getAttribute("data-type") === "submit") { // this. refers to the button just clicked and if its submit then action alert
+    // ceate an alert to say you clicked submit if submit clicked
+    alert("You clicked Submit!");
+ } else { // if button clicked is not submit
+    let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+ }
+        });
+    }
+});
+
 function runGame() {
     // Game logic here
 }
