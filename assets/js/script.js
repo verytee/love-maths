b4aaa6b0cc4bffebd694e529ec1660d277c85bce
Verity Allison <verityalls@gmail.com>
2025-12-15 13:43:40 +0000

    add function template

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+    // Game logic here
+}
+
+function checkAnswer() {
+    // Answer checking logic here
+}
+
+function calculateCorrectAnswer() {
+    // Calculation logic here
+}
+
+function incrementScore() {
+    // Score increment logic here
+}
+
+function incrementWrongAnswer() {
+    // Wrong answer increment logic here
+}
+
+function displayAdditionQuestion() {
+    // Display addition question logic here
+}
+
+function displaySubtractQuestion() {
+    // Display subtraction question logic here
+}
+
+function displayMultiplyQuestion() {
+    // Display multiplication question logic here
+}   
+
+function displayDivisionQuestion() {
+    // Display division question logic here
+}
